7bffd6a8109c12731d4e1154808c148123854bf8
Wagner M. Alves <wagnermalves@protonmail.com>
2022-06-29 11:54:10 -0300

    Install and Configure Inertia + VueJS + Tailwind CSS

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-2xl text-blue-800">Welcome</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, app, props, plugin }) {
+    createApp({ render: () => h(app, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
