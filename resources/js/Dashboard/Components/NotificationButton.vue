858d23d6916eb3e5063476b5f3eb374bfa680ba4
Wagner M. Alves <92180335+wagner-m-alves@users.noreply.github.com>
2022-08-12 18:45:29 -0300

    Improvement in NotificationButton Component

--- resources/js/Dashboard/Components/NotificationButton.vue
+++ resources/js/Dashboard/Components/NotificationButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button class="w-full h-full font-thin hover:text-blue-700 flex items-center justify-center dark:text-white dark:hover:text-blue-700" @click="action">
         <i class="far fa-bell"></i>
-        <span v-if="hasNotification" class="relative flex h-2 w-2 -top-1 -left-1">
+        <span v-if="hasNotification" class="relative flex h-2 w-2 -top-1.5 -left-1">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-600 opacity-75"></span>
             <span class="relative inline-flex rounded-full h-2 w-2 bg-red-600"></span>
         </span>
