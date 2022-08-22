9b519b051ec7eb2c366cd90587351eb9b544db78
Wagner M. Alves <92180335+wagner-m-alves@users.noreply.github.com>
2022-08-22 12:53:02 -0300

    Impove Breadcrumb Component

--- resources/js/Components/Breadcrumb.vue
+++ resources/js/Components/Breadcrumb.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="flex" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-2">
+        <ol class="flex flex-wrap items-center space-x-2 justify-center">
             <li class="flex items-center">
                 <span v-if="pages.length > 0" class="flex items-center text-gray-700 hover:text-gray-900 hover:cursor-pointer dark:text-gray-400 dark:hover:text-white" @click="go(main.route)"><i class="mb-1" :class="main.icon"></i></span>
                 <div v-else class="text-gray-400 dark:text-gray-500">
